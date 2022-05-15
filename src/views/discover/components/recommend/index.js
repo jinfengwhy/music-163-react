619b1c6d401280595f64e1e3a4d65a502f4264f8
jinfengwhy <xingzhe290@foxmail.com>
2022-05-15 15:58:54 +0800

    shallowEqual

--- src/views/discover/components/recommend/index.js
+++ src/views/discover/components/recommend/index.js
@@ -1,12 +1,13 @@
 import React, { memo, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 
 import { getTopBannersAction } from './store/actionCreators'
 
 const index = memo(props => {
+  // shallowEqual 浅层比较
   const { topBanners } = useSelector(state => ({
     topBanners: state.recommend.topBanners
-  }))
+  }), shallowEqual)
   const dispatch = useDispatch()
 
   useEffect(() => {
